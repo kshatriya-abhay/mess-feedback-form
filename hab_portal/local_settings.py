1ef2480a8d642b1f045d5cd6cca97962ea5ed06d
Rajas <rajasbhadke@gmail.com>
2017-12-02 13:56:50 +0530

    Change file:local_settings

--- hab_portal/local_settings.py
+++ hab_portal/local_settings.py
@@ -12,6 +12,7 @@ DATABASES = {
 DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR,"static")
 MEDIA_DIR = os.path.join(BASE_DIR,"media")
 
