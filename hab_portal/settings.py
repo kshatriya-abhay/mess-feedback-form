e66a50187b6890179b90bc5b16d06e34491f2a76
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2017-12-20 17:57:24 +0530

    init

--- hab_portal/settings.py
+++ hab_portal/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'hab_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hab_portal',
-        'USER': 'hab_portal_user',
-        'PASSWORD': 'SWC@giit',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
